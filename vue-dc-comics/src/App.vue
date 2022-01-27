12f53d514c5e1641477cf8219187269ed6f0cc84
perezalexreal <perezalexreal@gmail.com>
2022-01-27 17:10:47 +0100

    Header Finito

---
+++ vue-dc-comics/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <HeaderBox/>
+  </div>
+</template>
+
+<script>
+import HeaderBox from './components/HeaderBox.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderBox
+  }
+}
+</script>
+
+<style lang="scss">
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap'); 
+
+* {
+  padding: 0px ;
+  margin: 0px ;
+  box-sizing: border-box;
+font-family: 'Roboto', sans-serif;
+}
+</style>
---
+++ vue-dc-comics/src/components/HeaderBox.vue
@@ -0,0 +1,90 @@
+<template>
+    <header class="header">
+        <img class="logo-image" src="@/assets/dc-logo.png" alt="">
+        <div>
+            <ul>
+                <li>
+                    Characters
+                </li>
+                <li>
+                    Comics
+                </li>
+                <li>
+                    Movies
+                </li>
+                <li>
+                    TV
+                </li>
+                <li>
+                    Games
+                </li>
+                <li>
+                    Collectibles
+                </li>
+                <li>
+                    Videos
+                </li>
+                <li>
+                    Fans
+                </li>
+                <li>
+                    News
+                </li>
+                <li>
+                    Shop
+                </li>
+            </ul>
+        </div>
+    </header>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+.header {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    border: 1px solid gray;
+    padding-top: 40px;
+
+
+    img {
+        width: 70px;
+        height: 70px;
+        position: relative;
+        margin-top: -40px;
+    }
+    ul {
+        display: flex;
+        width: 100%;
+        height: 70px;
+
+        li {
+            width:100%;
+            list-style: none;
+            margin: 0px 10px;
+            text-align: center;
+            font-size: 12px;
+            font-weight: 900;
+            text-transform: uppercase;
+
+
+            &:hover {
+                  border-bottom: 1px solid blue;
+                  color: blue;
+            }
+          
+        }
+
+    }
+}
+
+</style>
